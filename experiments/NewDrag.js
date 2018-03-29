f9ffe4149f9a22485de5e8126760c988bdaea322
Audun Wigum Arbo <audunwar@stud.ntnu.no>
2018-03-29 15:18:01 +0200

    Add initial version of resizing

--- experiments/NewDrag.js
+++ experiments/NewDrag.js
@@ -1,3 +1,33 @@
+function calculateTransformOrigin(edges) {
+    let origin = "";
+    let topOrBottom = false;
+    let leftOrRight = false;
+    if (edges.top) {
+        origin += "bottom";
+        topOrBottom = true;
+    }
+    else if (edges.bottom) {
+        origin += "top";
+        topOrBottom = true;
+    }
+    if (edges.left) {
+        origin += " right";
+        leftOrRight = true;
+    }
+    else if (edges.right) {
+        origin += " left";
+        leftOrRight = true;
+    }
+
+    if (!topOrBottom) {
+        origin = "center" + origin;
+    }
+    else if (!leftOrRight) {
+        origin += " center";
+    }
+    return origin;
+}
+
 interact('.cell')
     .draggable({
         inertia: true,
@@ -19,8 +49,60 @@ interact('.cell')
             event.target.dataX = 0;
             event.target.dataY = 0;
         }
-    });
+    }).resizable({
+            // resize from all edges and corners
+            edges: {left: true, right: true, bottom: true, top: true},
 
+            // keep the edges inside the parent
+            restrictEdges: {
+                outer: 'parent',
+                endOnly: true,
+            },
+
+            // minimum size
+            restrictSize: {
+                min: {width: 100, height: 50},
+            },
+
+            inertia: true,
+    }).on('resizemove', function (event) {
+        const target = event.target;
+        let x = parseFloat(target.dataX || 0) + event.dx;
+        let y = parseFloat(target.dataY || 0) + event.dy;
+
+        // update the element's style
+        // target.style.width = event.rect.width + 'px';
+        // target.style.height = event.rect.height + 'px';
+        target.style.transformOrigin = calculateTransformOrigin(event.edges);
+        target.style.transform =
+            "scale(" + event.rect.width / event.target.clientWidth
+            + "," +  event.rect.height / event.target.clientHeight;
+
+        // translate when resizing from top or left edges
+        x += event.deltaRect.left;
+        y += event.deltaRect.top;
+        // target.style.transform += ' translate(' + x + 'px,' + y + 'px)';
+
+        target.dataX = x;
+        target.dataY = y;
+    }).on("resizeend", (event) => {
+        let element = document.elementsFromPoint(event.clientX, event.clientY).find((element) => element.classList.contains("empty"));
+
+        event.target.style.gridArea = calculateGridArea(event.target.style.gridArea, element.style.gridArea);
+        event.target.style["transform"] = "";
+});
+
+function calculateGridArea(oldArea, newArea) {
+    let oldAreaSplit = oldArea.split(" / ");
+    let newAreaSplit = newArea.split(" / ");
+    if (oldAreaSplit[0] > newAreaSplit[0] || oldAreaSplit[1] > newAreaSplit[1]) {
+        let temp = oldAreaSplit;
+        oldAreaSplit = newAreaSplit;
+        newAreaSplit = temp;
+    }
+    return oldAreaSplit[0] + " / " + oldAreaSplit[1]
+        + " / " + (parseInt(newAreaSplit[2]) + 1) + " / " + (parseInt(newAreaSplit[3]) + 1);
+}
 function dragMoveListener (event) {
     let target = event.target;
 
--- js/Interactions.js
+++ js/Interactions.js
@@ -22,7 +22,90 @@ interact('.cell')
 
             Application.layout.saveLayout();
         }
-    });
+    }).resizable({
+    // resize from all edges and corners
+    edges: {left: true, right: true, bottom: true, top: true},
+
+    // keep the edges inside the parent
+    restrictEdges: {
+        outer: 'parent',
+        endOnly: true,
+    },
+
+    // minimum size
+    restrictSize: {
+        min: {width: 100, height: 50},
+    },
+
+    inertia: true,
+}).on('resizemove', function (event) {
+    const module = Application.getModuleById(event.target.id);
+    const target = event.target;
+    let x = parseFloat(target.dataX || 0) + event.dx;
+    let y = parseFloat(target.dataY || 0) + event.dy;
+
+    // update the element's style
+    // target.style.width = event.rect.width + 'px';
+    // target.style.height = event.rect.height + 'px';
+    target.style.transformOrigin = calculateTransformOrigin(event.edges);
+    module.style.transformOrigin = calculateTransformOrigin(event.edges);
+    target.style.transform =
+        "scale(" + event.rect.width / event.target.clientWidth
+        + "," +  event.rect.height / event.target.clientHeight;
+
+    module.style.transform =
+        "scale(" + event.rect.width / event.target.clientWidth
+        + "," +  event.rect.height / event.target.clientHeight;
+}).on("resizeend", (event) => {
+    let element = document.elementsFromPoint(event.clientX, event.clientY).find((element) => element.classList.contains("empty"));
+    const module = Application.getModuleById(event.target.id);
+
+    module.area = calculateGridArea(event.target.style.gridArea, element.style.gridArea);
+    module.style["transform"] = "";
+    Application.layout.saveLayout();
+});
+
+function calculateGridArea(oldArea, newArea) {
+    let oldAreaSplit = oldArea.split(" / ");
+    let newAreaSplit = newArea.split(" / ");
+    if (oldAreaSplit[0] > newAreaSplit[0] || oldAreaSplit[1] > newAreaSplit[1]) {
+        let temp = oldAreaSplit;
+        oldAreaSplit = newAreaSplit;
+        newAreaSplit = temp;
+    }
+    return oldAreaSplit[0] + " / " + oldAreaSplit[1]
+        + " / " + (parseInt(newAreaSplit[2]) + 1) + " / " + (parseInt(newAreaSplit[3]) + 1);
+}
+
+function calculateTransformOrigin(edges) {
+    let origin = "";
+    let topOrBottom = false;
+    let leftOrRight = false;
+    if (edges.top) {
+        origin += "bottom";
+        topOrBottom = true;
+    }
+    else if (edges.bottom) {
+        origin += "top";
+        topOrBottom = true;
+    }
+    if (edges.left) {
+        origin += " right";
+        leftOrRight = true;
+    }
+    else if (edges.right) {
+        origin += " left";
+        leftOrRight = true;
+    }
+
+    if (!topOrBottom) {
+        origin = "center" + origin;
+    }
+    else if (!leftOrRight) {
+        origin += " center";
+    }
+    return origin;
+}
 
 function dragMoveListener (event) {
     const module = Application.getModuleById(event.target.id);
