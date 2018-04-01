67b8346d5626d9632f3088943618971a451d6b8c
Audun Wigum Arbo <audunwar@stud.ntnu.no>
2018-04-01 12:23:59 +0200

    Add ESLint to WebStorm config, create new ESLint config

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    },
+    "globals": {
+        "m": true,
+        "interact": true,
+        "SmoothieChart": true,
+        "TimeSeries": true
+    }
+};
