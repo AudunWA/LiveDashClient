ce02babd9c63813dbd01aade1eff652323acb764
Audun Wigum Arbo <audunwar@stud.ntnu.no>
2018-04-16 15:39:23 +0200

    Greatly improve linear gauge

--- js/modules/LinearGauge.js
+++ js/modules/LinearGauge.js
@@ -7,25 +7,29 @@ export class LinearGauge extends Module {
     constructor(id, area) {
         super(id, area);
         this.percentage = 0;
+        this.value = 0;
     }
 
     view() {
-        return m(".", Object.assign({
-            id: this.id,
-            class: this.classNames,
-            style: this.style,
-            onmouseenter: () => this.hovering = true,
-            onmouseleave: () => this.hovering = false
-        }, this.domAttributes),
-        m(".linear-gauge",
-            m(".gauge-percentage", {style: {width: `${this.percentage}%`}})
-        ),
-        this.editControls()
+        return m(".fc", Object.assign({
+                id: this.id,
+                class: this.classNames,
+                style: this.style,
+                onmouseenter: () => this.hovering = true,
+                onmouseleave: () => this.hovering = false
+            }, this.domAttributes),
+            m(".linear-gauge",
+                m(".gauge-percentage", {style: {width: `${this.percentage}%`}}),
+                m("span.gauge-text", this.value.toFixed(1) + "%")
+            ),
+            m("span.gauge-title", this.channel),
+            this.editControls()
         );
     }
 
     onData(value) {
         this.percentage = value / (MAX_SPEED - MIN_SPEED) * 100;
+        this.value = value;
     }
 
     onClick(event) {
